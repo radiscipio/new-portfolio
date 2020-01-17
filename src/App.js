c7598f9f897ffa8ba1d23973e7b5e0763740a3b3
Radiscipio <robertdiscipio@gmail.com>
2020-01-17 17:24:27 -0500

    Background image on home

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="navbar">
+        <div className="logo">Robert di Scipio</div>
+        <div className="links-container">
+          <div className="links">About</div>
+          <div className="links">Projects</div>
+          <div className="links">Contact</div>
+          <div className="links">Resume</div>
+        </div>
+      </div>
       <div className="greeting">
         <div>Hey there, I'm <span>Robert di Scipio</span></div>
         <div>I'm a Software Engineer</div>
